bb1fcc3c421105deb2a25f146412181b85cae297
Gábor Ziegler <orrmany@gmail.com>
2020-01-23 12:15:32 +0100

    Adding support for s340 SD
    Adding readme.txt to describe the way to obtain S340 and how to put into the bootloader. Fixing BLEDIS name.

--- src/usb/uf2/uf2cfg.h
+++ src/usb/uf2/uf2cfg.h
@@ -9,7 +9,11 @@
 #define FLASH_SIZE         (USER_FLASH_END-USER_FLASH_START) // Max flash size
 
 // Only allow to write application TODO dynamic depending on SD size
+#ifdef S340 //S340 uses more flash
+#define USER_FLASH_START   0x31000
+#else
 #define USER_FLASH_START   0x26000
+#endif
 #define USER_FLASH_END     0xAD000 // Fat Fs start here
 
 #define FLASH_PAGE_SIZE    4096
